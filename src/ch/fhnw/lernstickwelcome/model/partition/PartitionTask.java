8d68c5c49762aeb1004c5d157c80e58ed40cb96b
Sandro Schwager <sandro.schwager@students.fhnw.ch>
2017-03-02 08:19:29 +0100

    Added partition task for installations and information about partitions.

---
+++ src/ch/fhnw/lernstickwelcome/model/partition/PartitionTask.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.fhnw.lernstickwelcome.model.partition;
+
+import javafx.concurrent.Task;
+
+/**
+ *
+ * @author sschw
+ */
+public class PartitionTask extends Task<Boolean> {
+
+    @Override
+    protected Boolean call() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
