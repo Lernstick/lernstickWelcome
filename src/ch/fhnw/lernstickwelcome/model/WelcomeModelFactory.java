bb9501313ac985eddc38bc52e7d911d6edfa8deb
Patric Steiner <patric.steiner1@gmail.com>
2017-03-19 23:06:22 +0900

    added xml to configure applications and a factory method to retrieve applications

--- src/ch/fhnw/lernstickwelcome/model/WelcomeModelFactory.java
+++ src/ch/fhnw/lernstickwelcome/model/WelcomeModelFactory.java
@@ -7,15 +7,26 @@ package ch.fhnw.lernstickwelcome.model;
 
 import ch.fhnw.lernstickwelcome.model.application.ApplicationGroupTask;
 import ch.fhnw.lernstickwelcome.model.application.ApplicationTask;
+import ch.fhnw.lernstickwelcome.model.application.AptGetPackages;
+import ch.fhnw.lernstickwelcome.model.application.CombinedPackages;
+import ch.fhnw.lernstickwelcome.model.application.WgetPackages;
 import ch.fhnw.lernstickwelcome.model.proxy.ProxyTask;
+import ch.fhnw.lernstickwelcome.model.WelcomeUtil;
 import ch.fhnw.util.ProcessExecutor;
 import ch.fhnw.util.StorageDevice;
 import ch.fhnw.util.StorageTools;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.freedesktop.dbus.exceptions.DBusException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * XXX Change this class if applications should be load from file
@@ -68,4 +79,48 @@ public class WelcomeModelFactory {
         return new ProxyTask();
     }
     // TODO add more categories which should be initialized
+    
+    public static ApplicationTask getApplicationTask(String name) {
+    	File xmlFile = new File("applications.xml");
+    	Document xmlDoc = WelcomeUtil.parseXmlFile(xmlFile);
+    	NodeList applications = xmlDoc.getElementsByTagName("application");
+    	for (int i = 0; i < applications.getLength(); i++) {
+    		Node application = applications.item(i);
+    		if (application.getNodeType() == Node.ELEMENT_NODE) {
+    			Element app = (Element) application;
+    			if (app.getAttribute("name") == name) { // found application
+        			String description = app.getElementsByTagName("description").item(0).getNodeValue();
+        			String icon = app.getElementsByTagName("icon").item(0).getNodeValue();
+        			String helpPath = app.getElementsByTagName("help-path").item(0).getNodeValue();
+        			List<String> aptgetPackages = new ArrayList<>();
+        			List<String> wgetPackages = new ArrayList<>();
+        			String wgetFetchUrl;
+        			String wgetSaveDir;
+        			NodeList packages = app.getElementsByTagName("package");
+        			for (int j = 0; j < packages.getLength(); j++) {
+        				Element pkg = ((Element)packages.item(j));
+        				String type = pkg.getAttribute("type");
+        				String pkgName = pkg.getNodeValue();
+        				switch (type) {
+						case "aptget":
+							aptgetPackages.add(pkgName);
+							break;
+						case "wget":
+							wgetPackages.add(pkgName);
+	        				wgetFetchUrl = pkg.getAttribute("fetchUrl");
+	        				wgetSaveDir = pkg.getAttribute("saveDir");
+							break;
+						default: break;
+						}
+        			}
+        			CombinedPackages pkgs = new CombinedPackages(
+    					new AptGetPackages(aptgetPackages.toArray(new String[aptgetPackages.size()])), 
+    					new WgetPackages(wgetPackages.toArray(new String[wgetPackages.size()]), wgetFetchUrl, wgetSaveDir)
+        			);
+        			ApplicationTask task = new ApplicationTask(name, description, icon, helpPath, pkgs);
+        			return task;
+    			}
+    		}
+    	}
+    }
 }
