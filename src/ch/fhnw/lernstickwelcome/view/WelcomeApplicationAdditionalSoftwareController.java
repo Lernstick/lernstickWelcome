2e91889233f5eb26a37d03a0dc17c28d5186e9cc
LineStettler <line.stettler@students.fhnw.ch>
2017-03-19 01:19:13 +0100

    added fxmls for sdt lernstick wa

---
+++ src/ch/fhnw/lernstickwelcome/view/WelcomeApplicationAdditionalSoftwareController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.fhnw.lernstickwelcome.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class WelcomeApplicationAdditionalSoftwareController implements Initializable {
+
+    @FXML
+    private Button btn_as_help;
+    @FXML
+    private ToggleButton tbtn_as_teaching;
+    @FXML
+    private ToggleButton tbtn_as_skype;
+    @FXML
+    private ToggleButton tbtn_as_gComris1;
+    @FXML
+    private ToggleButton tbtn_as_gComris2;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onClickShowHelp(MouseEvent event) {
+    }
+    
+}
---
+++ src/ch/fhnw/lernstickwelcome/view/WelcomeApplicationSystemStdController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.fhnw.lernstickwelcome.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class WelcomeApplicationSystemStdController implements Initializable {
+
+    @FXML
+    private Button btn_sysStd_help;
+    @FXML
+    private TextField txt_sysStd_username;
+    @FXML
+    private ChoiceBox<?> choice_sysStd_visible_for;
+    @FXML
+    private CheckBox cb_sysStd_start_wa;
+    @FXML
+    private CheckBox cb_sysStd_show_warning;
+    @FXML
+    private CheckBox cb_sysStd_direct_sound;
+    @FXML
+    private CheckBox cb_sysStd_block_kde;
+    @FXML
+    private TextField txt_sysStd_exchange_partition;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onClickShowHelp(MouseEvent event) {
+    }
+    
+}
