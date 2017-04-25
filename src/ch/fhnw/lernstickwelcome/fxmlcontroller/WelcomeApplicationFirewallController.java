c5be06d6a824cb37ba613db85085003a22c9fc89
Sandro Schwager <sandro.schwager@students.fhnw.ch>
2017-04-25 11:03:38 +0200

    Added delete button and validation.

--- src/ch/fhnw/lernstickwelcome/fxmlcontroller/WelcomeApplicationFirewallController.java
+++ src/ch/fhnw/lernstickwelcome/fxmlcontroller/WelcomeApplicationFirewallController.java
@@ -5,12 +5,15 @@
  */
 package ch.fhnw.lernstickwelcome.fxmlcontroller;
 
+import ch.fhnw.lernstickwelcome.controller.exception.TableCellValidationException;
 import ch.fhnw.lernstickwelcome.controller.exception.ValidationException;
 import ch.fhnw.lernstickwelcome.model.firewall.IpFilter;
 import ch.fhnw.lernstickwelcome.model.firewall.WebsiteFilter;
 import ch.fhnw.lernstickwelcome.model.firewall.WebsiteFilter.SearchPattern;
 import ch.fhnw.lernstickwelcome.util.WelcomeUtil;
+import ch.fhnw.lernstickwelcome.view.impl.ButtonCell;
 import ch.fhnw.lernstickwelcome.view.impl.ToggleSwitch;
+import ch.fhnw.lernstickwelcome.view.impl.ValidatableTextFieldCell;
 import java.net.URL;
 import java.text.MessageFormat;
 import java.util.ResourceBundle;
@@ -45,6 +48,10 @@ public class WelcomeApplicationFirewallController implements Initializable {
     @FXML
     private TableColumn<WebsiteFilter, String> tab_fw_search_criteria;
     @FXML
+    private TableColumn<WebsiteFilter, WebsiteFilter> tab_fw_search_btn_edit_save;
+    @FXML
+    private TableColumn<WebsiteFilter, WebsiteFilter> tab_fw_search_btn_delete;
+    @FXML
     private ComboBox<WebsiteFilter.SearchPattern> choice_fw_search_pattern;
     @FXML
     private TextField txt_fw_search_criteria;
@@ -60,6 +67,10 @@ public class WelcomeApplicationFirewallController implements Initializable {
     private TableColumn<IpFilter, String> tab_fw_server_port;
     @FXML
     private TableColumn<IpFilter, String> tab_fw_server_desc;
+    @FXML
+    private TableColumn<WebsiteFilter, WebsiteFilter> tab_fw_server_btn_edit_save;
+    @FXML
+    private TableColumn<WebsiteFilter, WebsiteFilter> tab_fw_server_btn_delete;
     @FXML
     private Button btn_fw_add_new_server;
     @FXML
@@ -91,29 +102,53 @@ public class WelcomeApplicationFirewallController implements Initializable {
         tab_fw_search_criteria.setCellFactory(TextFieldTableCell.forTableColumn());
         tab_fw_search_criteria.setOnEditCommit(e -> e.getTableView().getItems().get(e.getTablePosition().getRow())
                         .searchCriteriaProperty().setValue(e.getNewValue()));
+        tab_fw_search_btn_edit_save.setCellFactory(c -> new ButtonCell(ButtonCell.Type.EDIT, c.getTableView()));
+        tab_fw_search_btn_delete.setCellFactory(p -> new ButtonCell(ButtonCell.Type.DELETE, p.getTableView()));
         
         // Set TableView IpFilter cell properties and implement edit functionality
         tab_fw_server_protocol.setCellValueFactory(p -> p.getValue().protocolProperty());
         tab_fw_server_protocol.setCellFactory(ComboBoxTableCell.forTableColumn(IpFilter.Protocol.values()));
         tab_fw_server_protocol.setOnEditCommit(e -> e.getTableView().getItems().get(e.getTablePosition().getRow())
                         .protocolProperty().setValue(e.getNewValue()));
-        tab_fw_server_ip.setCellFactory(TextFieldTableCell.forTableColumn());
-        tab_fw_server_ip.setCellValueFactory(p -> p.getValue().ipAddressProperty());
-        tab_fw_server_ip.setOnEditCommit(e -> e.getTableView().getItems().get(e.getTablePosition().getRow())
+        tab_fw_server_ip.setCellFactory(ValidatableTextFieldCell.forTableColumn((s, i) -> {
+            try {
+                WelcomeUtil.checkTarget((String) s, i);
+                return null;
+            } catch(TableCellValidationException ex) {
+                return ex;
+            }
+        }, rb));
+        tab_fw_server_ip.setOnEditCommit(e -> 
+                e.getTableView().getItems().get(e.getTablePosition().getRow())
                         .ipAddressProperty().setValue(e.getNewValue()));
+        tab_fw_server_ip.setCellValueFactory(p -> p.getValue().ipAddressProperty());
+        tab_fw_server_port.setCellFactory(ValidatableTextFieldCell.forTableColumn((s, i) -> {
+            try {
+                WelcomeUtil.checkPortRange((String) s, i);
+                return null;
+            } catch(TableCellValidationException ex) {
+                return ex;
+            }
+        }, rb));
         tab_fw_server_port.setCellValueFactory(p -> p.getValue().portProperty());
-        tab_fw_server_port.setCellFactory(TextFieldTableCell.forTableColumn());
-        tab_fw_server_port.setOnEditCommit(e -> e.getTableView().getItems().get(e.getTablePosition().getRow())
+        tab_fw_server_port.setOnEditCommit(e -> 
+                e.getTableView().getItems().get(e.getTablePosition().getRow())
                         .portProperty().setValue(e.getNewValue()));
         tab_fw_server_desc.setCellValueFactory(p -> p.getValue().descriptionProperty());
         tab_fw_server_desc.setCellFactory(TextFieldTableCell.forTableColumn());
         tab_fw_server_desc.setOnEditCommit(e -> e.getTableView().getItems().get(e.getTablePosition().getRow())
                         .descriptionProperty().setValue(e.getNewValue()));
+        tab_fw_server_btn_edit_save.setCellFactory(c -> new ButtonCell(ButtonCell.Type.EDIT, c.getTableView()));
+        tab_fw_server_btn_delete.setCellFactory(p -> new ButtonCell(ButtonCell.Type.DELETE, p.getTableView()));
         
         // Load ComboBox data
         choice_fw_search_pattern.setConverter(getSearchPatternStringConverter());
         choice_fw_search_pattern.getItems().addAll(WebsiteFilter.SearchPattern.values());
         choice_fw_protocol.getItems().addAll(IpFilter.Protocol.values());
+        
+        // FIXME Edit/Save button doesn't work - Useful for touch screen
+        tab_fw_search_btn_edit_save.setVisible(false);
+        tab_fw_server_btn_edit_save.setVisible(false);
     }
     
     private StringConverter<SearchPattern> getSearchPatternStringConverter() {
@@ -218,18 +253,18 @@ public class WelcomeApplicationFirewallController implements Initializable {
         if (choice_fw_protocol.getSelectionModel().isEmpty()) return false;
         try {
             WelcomeUtil.checkTarget(txt_fw_new_ip.getText(), -1);
-            txt_fw_new_ip.setStyle("");
+            txt_fw_new_ip.getStyleClass().remove("error");
         } catch(ValidationException ex) {
             txt_fw_new_ip.setPromptText(MessageFormat.format(rb.getString(ex.getMessage()), ex.getMessageDetails()));
-            txt_fw_new_ip.setStyle("-fx-border-color: red ; -fx-border-width: 2px ;");
+            txt_fw_new_ip.getStyleClass().add("error");
             return false;
         }
         try {
             WelcomeUtil.checkPortRange(txt_fw_new_port.getText(), -1);
-            txt_fw_new_ip.setStyle("");
+            txt_fw_new_ip.getStyleClass().remove("error");
         } catch(ValidationException ex) {
             txt_fw_new_port.setPromptText(MessageFormat.format(rb.getString(ex.getMessage()), ex.getMessageDetails()));
-            txt_fw_new_port.setStyle("-fx-border-color: red ; -fx-border-width: 2px ;");
+            txt_fw_new_ip.getStyleClass().add("error");
             return false;
         }
         return true;
