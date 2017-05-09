ae54904b3fe2d0b2280bc98354a4bba1fdb1442d
Roger Obrist <roger.obrist@students.fhnw.ch>
2017-05-09 10:38:18 +0200

    changed values on std version system startup time

--- src/ch/fhnw/lernstickwelcome/fxmlcontroller/WelcomeApplicationSystemStdController.java
+++ src/ch/fhnw/lernstickwelcome/fxmlcontroller/WelcomeApplicationSystemStdController.java
@@ -28,7 +28,7 @@ import javafx.util.StringConverter;
  */
 public class WelcomeApplicationSystemStdController implements Initializable {
 
-    private final Integer[] visibleForValues = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    private final Integer[] visibleForValues = new Integer[] { 5, 10, 15, 20, 25, 30, 40, 50, 60 };
 
     
     @FXML
