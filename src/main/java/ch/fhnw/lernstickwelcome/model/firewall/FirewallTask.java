e8f549fdf48043481a5e09fe3b85a7539889fd9f
Sandro Schwager <sandro.schwager@students.fhnw.ch>
2017-06-06 14:55:48 +0200

    Removed unused imports.

--- src/main/java/ch/fhnw/lernstickwelcome/model/firewall/FirewallTask.java
+++ src/main/java/ch/fhnw/lernstickwelcome/model/firewall/FirewallTask.java
@@ -25,8 +25,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
